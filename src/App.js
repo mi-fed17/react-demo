8b3dfe1b10a49c6cbad050738cec1f4dea73b293
Jesper Orb <jesper.orb@zocom.se>
2018-05-07 09:13:43 +0200

    Header components

--- src/App.js
+++ src/App.js
@@ -4,10 +4,28 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Header greeting="Hello" size={1} />
+
+        <SecondHeader>
+          Hello { /* props.children*/ }
+        </SecondHeader>
       </div>
-      );
+    );
   }
 }
 
 export default App;
+
+function Header(props) {
+  if(props.size === 1 ){
+    return <h1> { props.greeting } </h1>;
+  }
+  return <h2> { props.greeting } </h2>;
+}
+
+/**
+ * Props children is unique
+ */
+function SecondHeader(props){
+  return <h1> { props.children } </h1>
+}
