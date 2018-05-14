fa8f1123eaa74b9841679e5f433a8198dabe5fed
Jesper Orb <jesper.orb@zocom.se>
2018-05-14 09:59:35 +0200

    Add state

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 
 class LoginForm extends Component {
 
+  state = {
+    email: '',
+    password: '',
+    error: ''
+  }
+  
+  handleEmail = (event) => {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePassword = (event) => {
+    this.setState({ password: event.target.value });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <form>
         <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <label htmlFor="email">Email address</label>
+          <input 
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                  onChange={this.handleEmail}
+                  value={this.state.email}
+          />
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+          <input 
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  onChange={this.handlePassword}
+                  value={this.state.password}
+          />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
